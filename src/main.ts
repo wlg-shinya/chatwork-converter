2c2e11614571f390eecaa5cf9f80aab06b9246b4
s-watanabe <s-watanabe@three-rings.net>
2023-10-19 16:16:19 +0900

    add vue-axios

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+createApp(App)
+    .use(VueAxios, axios)
+    .mount('#app')
+
