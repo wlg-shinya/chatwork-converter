db062193ba62e09a39ffb614bac21ff4f9278fa0
s-watanabe <s-watanabe@three-rings.net>
2023-10-19 18:44:41 +0900

    chatworkの実際のメッセージリンクを指定してメッセージを取得できるように

--- src/components/ChatConverter.vue
+++ src/components/ChatConverter.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
+import { ref } from "vue"
 import axios from "axios"
 
+const messageURL = ref("")
+
 function test() {
+  const url = messageURL.value
+  if (!url) return
+
+  const match = url.match(/.*rid([0-9]+)-([0-9]+)/)
+  if (!match) return 
+
+  const roomId = match[1]
+  const messageId = match[2]
+
   axios
     .get("/api/chatwork_get_message", {
       params: {
-        room_id: "338454030",
-        message_id: "1766718668491759616"
+        room_id: roomId,
+        message_id: messageId
       },
     })
     .then((response) => {
@@ -21,6 +33,7 @@ function test() {
 
 <template>
   <div>
+    <input v-model="messageURL" />
     <button @click="test()">test</button>
   </div>
 </template>
