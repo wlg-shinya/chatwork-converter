470bd1dfc0402184a0218b66274d6f0c1c22863c
s-watanabe <s-watanabe@three-rings.net>
2023-10-19 18:30:11 +0900

    chatworkからメッセージ取得テスト（成功）

--- src/components/ChatConverter.vue
+++ src/components/ChatConverter.vue
@@ -1,6 +1,21 @@
 <script setup lang="ts">
+import axios from "axios"
+
 function test() {
-  console.log("test")
+  axios
+    .get("/api/chatwork_get_message", {
+      params: {
+        room_id: "338454030",
+        message_id: "1766718668491759616"
+      },
+    })
+    .then((response) => {
+      const data = JSON.parse(JSON.stringify(response.data))
+      console.log(data)
+    })
+    .catch((err) => {
+      throw err
+    })
 }
 </script>
 
