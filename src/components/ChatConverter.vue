bf6fb56427e58e6006ffea951a6c268c1a141443
s-watanabe <s-watanabe@three-rings.net>
2023-10-20 10:38:19 +0900

    指定メッセージから指定数のメッセージをまとめて特定できるように

--- src/components/ChatConverter.vue
+++ src/components/ChatConverter.vue
@@ -9,10 +9,11 @@ function test() {
   if (!url) return
 
   const match = url.match(/.*rid([0-9]+)-([0-9]+)/)
-  if (!match) return 
+  if (!match) return
 
   const roomId = match[1]
   const messageId = match[2]
+  const count = 5
 
   axios
     .get("/api/chatwork_get_messages", {
@@ -22,9 +23,22 @@ function test() {
     })
     .then((response) => {
       const data = JSON.parse(JSON.stringify(response.data))
-      console.log(data)
+      let startIndex = -1
+      // 指定メッセージがどこにあるか特定する
+      data.some((x: any, i: number) => {
+        if (x.message_id == messageId) {
+          startIndex = i
+          return true
+        } else {
+          return false
+        }
+      })
+      // 指定メッセージから指定数分のメッセージを収集
+      const endIndex = startIndex + count < data.length ? startIndex + count : data.length
+      const targetMessage = data.slice(startIndex, endIndex)
+      console.log(targetMessage)
     })
-    .catch((err) => {
+    .catch((err: any) => {
       throw err
     })
 }
