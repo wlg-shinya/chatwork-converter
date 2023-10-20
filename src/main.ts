7e0c09cf13093a4d7b9a736f7e4f3029c46a2e1b
s-watanabe <s-watanabe@three-rings.net>
2023-10-20 16:14:25 +0900

    add font awesome

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,15 @@ import VueAxios from 'vue-axios'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+// fort awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+
 createApp(App)
     .use(VueAxios, axios)
+    .component('fa', FontAwesomeIcon)
     .mount('#app')
 
