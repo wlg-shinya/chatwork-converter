5ab8b06f746289e45290213b3f7169af17d4ebcc
s-watanabe <s-watanabe@three-rings.net>
2023-10-20 15:13:21 +0900

    bootstrap使う準備不足の修正

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 // bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 createApp(App)
