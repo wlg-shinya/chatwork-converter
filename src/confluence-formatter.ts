161faaaec573c836befefec3e85ea2b8c10883c7
s-watanabe <s-watanabe@three-rings.net>
2023-10-23 09:53:19 +0900

    ペースト方法もフォーマッタ側に引っ越し

--- src/confluence-formatter.ts
+++ src/confluence-formatter.ts
@@ -1,6 +1,9 @@
 import Formatter from "@/formatter"
 
 export default class ConfluenceFormatter implements Formatter {
+    howToPaste() {
+        return "Confluence編集画面にて挿入したい場所で＋ボタン押下後マークアップを選択。表示された画面で\"ConfluenceWiki\"を選択してペーストしてください"
+    }
     link(link: string, text = "") {
         return text ? `[${text}|${link}]` : `[${link}]`
     }
--- src/formatter.ts
+++ src/formatter.ts
@@ -1,4 +1,5 @@
 export default interface Formatter {
+    howToPaste(): string
     link(link: string, text: string): string
     bold(text: string): string
     separator(): string
