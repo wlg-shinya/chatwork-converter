f725dece84341161fc9e977f826b08d702fc8d41
s-watanabe <s-watanabe@three-rings.net>
2023-11-02 11:43:49 +0900

    frontend -> bakcendのプロキシエラー対処。代わりにローカルでは動作しなくなります

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,13 @@ const { defineConfig } = require('@vue/cli-service')
 process.env.VUE_APP_VERSION = process.env.npm_package_version
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://backend:3000'
+      }
+    }
+  },
   pluginOptions: {
     express: {
       shouldServeApp: true,
