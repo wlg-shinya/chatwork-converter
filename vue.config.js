5d6f7770db51acf3d4fc7cc5a795057a2272d516
s-watanabe <s-watanabe@three-rings.net>
2023-11-06 17:33:43 +0900

    公開して問題ない環境変数を引っ越し

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
+
+// 公開環境変数
 process.env.VUE_APP_VERSION = process.env.npm_package_version
+process.env.VUE_APP_BASE_URL = "http://localhost"
+process.env.VUE_APP_BACKEND_URL = "http://localhost:3000"
+process.env.VUE_APP_TITLE = "Chatworkコンバーター"
+process.env.VUE_APP_STORAGE_KEY = "tr-chatconv"
+
+// production時は上書きする設定
+if (process.env.NODE_ENV == "production") {
+  process.env.VUE_APP_BASE_URL = "http://localhost"
+  process.env.VUE_APP_BACKEND_URL = "http://localhost:3000"
+}
+
 module.exports = defineConfig({
   transpileDependencies: true,
   pluginOptions: {
