3916842102c86479d906f95d161144e70e64d9bc
s-watanabe <s-watanabe@three-rings.net>
2023-11-07 11:08:18 +0900

    整理

--- src/global-settings.ts
+++ src/global-settings.ts
@@ -1,6 +1,7 @@
+const BASE_URL = process.env.NODE_ENV === "production" ? "http://127.0.0.1" : "http://localhost"
 const GlobalSettings = {
-    baseUrl: process.env.NODE_ENV === "production" ? "http://127.0.0.1" : "http://localhost",
-    backendUrl: process.env.NODE_ENV === "production" ? "http://127.0.0.1:3000" : "http://localhost:3000",
+    baseUrl: BASE_URL,
+    backendUrl: `${BASE_URL}:3000`,
     title: "Chatworkコンバーター",
     storageKey: "tr-chatconv",
 }
