94f0eb06108bc796cf5f0febc01a518458b28936
s-watanabe <s-watanabe@three-rings.net>
2023-12-26 12:58:42 +0900

    vite.config整理

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+// https://vitejs.dev/config/
 export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  const htmlPlugin = () => ({
-    name: "html-transform",
-    transformIndexHtml: (html: string) => html.replace(/%=(.*?)%/g, (match, p1) => env[p1] ?? match),
-  });
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
-    plugins: [vue(), htmlPlugin()],
+    plugins: [vue()],
     server: {
       port: Number(process.env.VITE_BASE_PORT),
     },
