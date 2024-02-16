213750375b94b286b5592babe0524bbd8438592f
s-watanabe <s-watanabe@three-rings.net>
2024-02-16 20:01:50 +0900

    テーブルの見た目調整

--- src/formatter/html-formatter.ts
+++ src/formatter/html-formatter.ts
@@ -16,14 +16,21 @@ class HtmlFormatter implements Formatter {
   }
   body(text: string) {
     let result = text;
+    // 各種タグ変換
+    const tableTag = "<table class='table table-bordered'>";
     result = ChatworkTagConverter.to(result, (_match, ...p) => `<span>【To】${p[0]}</span>`);
     result = ChatworkTagConverter.toall(result, (_match, ...p) => `<span>【ToALL】${p[0]}</span>`);
     result = ChatworkTagConverter.reply(result, (_match, ...p) => `<span>【Re】${p[0]}</span>`);
     result = ChatworkTagConverter.quote(result, (_match, ...p) => `<blockquote>${p[0]}</blockquote>`);
     result = ChatworkTagConverter.fileUploaded(result, (_match, ...p) => `<span>${p[0]}</span>`);
     result = ChatworkTagConverter.task(result, (_match, ...p) => `<div><div>【${p[0]}】</div><span>${p[1]}</span></div>`);
-    result = ChatworkTagConverter.infoWithTitle(result, (_match, ...p) => `<div><div>【${p[0]}】</div><span>${p[1]}</span></div>`);
-    result = ChatworkTagConverter.info(result, (_match, ...p) => `<div>${p[0]}</div>`);
+    result = ChatworkTagConverter.infoWithTitle(
+      result,
+      (_match, ...p) => `${tableTag}<thead><tr><th>${p[0]}</th></tr></thead><tbody><tr><td>${p[1]}</td></tr></tbody></table>`
+    );
+    result = ChatworkTagConverter.info(result, (_match, ...p) => `${tableTag}<tbody><tr><td>${p[0]}</td></tr></tbody></table>`);
+    // 改行 -> <br>
+    result = result.replace(/\n/g, "<br>");
     result = `<div>${result}</div>`;
     return result;
   }
